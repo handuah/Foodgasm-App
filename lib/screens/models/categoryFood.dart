45b3cb6f02d86caee2d010ee66f809c88f0673d2
handuah <handuah@gmail.com>
2021-08-09 12:27:48 +0000

    created category food class

---
+++ lib/screens/models/categoryFood.dart
@@ -0,0 +1,10 @@
+class CategoryFood {
+  final String imagePath;
+  final String topColor;
+
+  final String bottomColor;
+  final String categoryName;
+
+  CategoryFood(
+      this.imagePath, this.topColor, this.bottomColor, this.categoryName);
+}
