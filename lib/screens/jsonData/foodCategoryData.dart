4b2dc9f264bc4856e2f9c934da2cf7a512acd6fb
handuah <handuah@gmail.com>
2021-08-09 13:38:42 +0000

    changed background images

--- lib/screens/jsonData/foodCategoryData.dart
+++ lib/screens/jsonData/foodCategoryData.dart
@@ -9,27 +9,30 @@ class FoodCategoryData {
       "categoryName": "Chinese",
     },
     {
-      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
-      "topColor": "#DB6B7B",
-      "bottomColor": "#D89063",
+      "imagePath":
+          "https://media-cdn.tripadvisor.com/media/photo-s/09/17/2b/5b/grand-beirut-restaurant.jpg",
+      "topColor": "#E87990",
+      "bottomColor": "#9F64E6",
       "categoryName": "Lebanese",
     },
     {
-      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
-      "topColor": "#DB6B7B",
-      "bottomColor": "#D89063",
+      "imagePath":
+          "https://lh3.googleusercontent.com/proxy/R1QZHTaOL1EShPUcE5aS1KUSt35B8sJal90M8U3V90o2tytQrTG7xgvth7vaBoPVbaB2eirQptiQRXTCpTbbNlCiGWo_NWbnfDHvQjHy2BkwdAFPKHOdq7DdznycyltDE0LJeYmDAXS_aZb6hJE",
+      "topColor": "#2384A8",
+      "bottomColor": "#2A35AB",
       "categoryName": "Korean",
     },
     {
-      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "imagePath":
+          "https://assets.site-static.com/userFiles/1621/image/italian-food-in-durango.jpg",
       "topColor": "#DB6B7B",
       "bottomColor": "#D89063",
       "categoryName": "Italian",
     },
     {
-      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
-      "topColor": "#DB6B7B",
-      "bottomColor": "#D89063",
+      "imagePath": "https://i.insider.com/5ae36708bd967148008b461f?width=700",
+      "topColor": "#E87990",
+      "bottomColor": "#9F64E6",
       "categoryName": "Mexican",
     },
   ];
