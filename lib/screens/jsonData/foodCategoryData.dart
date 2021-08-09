55800fb98b9e0ecbfea34edcf044aea7814280ca
handuah <handuah@gmail.com>
2021-08-09 12:55:48 +0000

    created foodCategory json

---
+++ lib/screens/jsonData/foodCategoryData.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/material.dart';
+
+class FoodCategoryData {
+  static final getData = [
+    {
+      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "topColor": "#DB6B7B",
+      "bottomColor": "#D89063",
+      "categoryName": "Chinese",
+    },
+    {
+      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "topColor": "#DB6B7B",
+      "bottomColor": "#D89063",
+      "categoryName": "Lebanese",
+    },
+    {
+      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "topColor": "#DB6B7B",
+      "bottomColor": "#D89063",
+      "categoryName": "Korean",
+    },
+    {
+      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "topColor": "#DB6B7B",
+      "bottomColor": "#D89063",
+      "categoryName": "Italian",
+    },
+    {
+      "imagePath": "https://media.timeout.com/images/105441129/image.jpg",
+      "topColor": "#DB6B7B",
+      "bottomColor": "#D89063",
+      "categoryName": "Mexican",
+    },
+  ];
+}
