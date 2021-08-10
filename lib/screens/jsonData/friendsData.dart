2248f7d6d8904f637e87d999e83d2ec5ba71f841
handuah <handuah@gmail.com>
2021-08-10 15:41:58 +0000

    init commit of circle avatar url

---
+++ lib/screens/jsonData/friendsData.dart
@@ -0,0 +1,38 @@
+class SuggestedFriends {
+  static final getFriendsImages = [
+    {
+      "imageUrl":
+          "https://static.wikia.nocookie.net/the-bangtan-boys/images/b/bf/Jungkook_Butter_Album_Version_4.jpg/revision/latest?cb=20210630151101",
+    },
+    {
+      "imageUrl":
+          "https://i.pinimg.com/originals/c5/7a/44/c57a442f17f35226d7430dd62bda5e48.png",
+    },
+    {
+      "imageUrl":
+          "https://kenyabtsarmy.files.wordpress.com/2021/05/bts-jimin-trending-on-twitter-1.jpg?w=1024",
+    },
+    {
+      "imageUrl":
+          "https://w0.peakpx.com/wallpaper/521/841/HD-wallpaper-rm-bts-festa-2021-butter-kpop-festa2021-kim-namjoon-bangtan-sonyeondan-namjoon-rap-monster.jpg",
+    },
+    {
+      "imageUrl":
+          "https://qph.fs.quoracdn.net/main-qimg-05933300376d4804c9f57b3262130a3d",
+    },
+    {
+      "imageUrl":
+          "https://w7w5t4b3.rocketcdn.me/wp-content/uploads/2020/09/ENHYPEN-member-Jake.jpeg",
+    },
+    {
+      "imageUrl": "https://pbs.twimg.com/media/Ei_UA8YUMAAU4JR.jpg",
+    },
+    {
+      "imageUrl": "https://pbs.twimg.com/media/Enoy2iKVcAEabZO.jpg",
+    },
+    {
+      "imageUrl":
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9CNx153ZCsSeUnZCC1THMI77Yo6sfsa9izA&usqp=CAU",
+    },
+  ];
+}
