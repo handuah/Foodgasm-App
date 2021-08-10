c3cb56b72cd0c1d018077eea6912487b76793414
handuah <handuah@gmail.com>
2021-08-10 14:32:00 +0000

    initial commit

---
+++ lib/screens/jsonData/topRestaurantsData.dart
@@ -0,0 +1,34 @@
+class TopRestaurantsData {
+  static final getRestaurants = [
+    {
+      "openState": "OPEN",
+      "ratings": "3.5",
+      "imagePath":
+          "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fnickpassmore%2Ffiles%2F2015%2F07%2Fchinese.jpg",
+      "restaurantName": "TipTop Chinese",
+      "country": "Chinese",
+      "distance": "0.4km",
+      "address": "10th Crescent Road, East - Legon"
+    },
+    {
+      "openState": "OPEN",
+      "ratings": "4.5",
+      "imagePath":
+          "https://cf.bstatic.com/data/xphoto/1182x887/222/22281452.jpg?size=S",
+      "restaurantName": "Patisserie",
+      "country": "Italian",
+      "distance": "1.0km",
+      "address": "2nd Osu Konor Street, Obo, Kwahu"
+    },
+    {
+      "openState": "CLOSED",
+      "ratings": "2.5",
+      "imagePath":
+          "https://static.toiimg.com/thumb/65489383.cms?width=1200&height=900",
+      "restaurantName": "Taj Mahal Expo",
+      "country": "Indian",
+      "distance": "0.2km",
+      "address": "No. 1 Lebanon Road, James Town, Accra"
+    },
+  ];
+}
